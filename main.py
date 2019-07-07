d842f526c8dc8ba9e45bf11209a44538b0974005
fry10 <pgmhecate5@gmail.com>
2019-07-07 17:21:04 +0900

    類似単語検索機能を実装

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import pickle
 import os
 
-def cmd(mes,ls):
+def cmd(mes,ls,ka):
     if mes == "del":
         delName = input("デリートネーム：")
         delItem = [i for i in ls if "".join(i[0]) == delName]
@@ -13,16 +13,102 @@ def cmd(mes,ls):
         else:
             print("No data")
         print("")
-        name = input("名前:")
-        return cmd(name,ls)
+        name = input("登録:")
+        return cmd(name,ls,ka)
     elif mes == "print":
         for i in ls:
             print(i)
         print("")
+        name = input("登録:")
+        return cmd(name,ls,ka)
+    elif mes == "eval":
+        ka.setMode('H', 'a')
+        conv = ka.getConverter()
+        word1 = conv.do(input("word1:"))
+        word2 = conv.do(input("word2:"))
+        print("Score({},{}): {}".format(word1,word2,evalScore(word1,word2)))
+        print("")
         name = input("名前:")
-        return cmd(name,ls)
+        return cmd(name,ls,ka)
+    elif mes == "search":
+        ka.setMode('H', 'H')
+        conv = ka.getConverter()
+        word = conv.do(input("サーチワード: "))
+        while(True):
+            print(word)
+            wordls = []
+            ka.setMode('H', 'a')
+            conv = ka.getConverter()
+            for i in word:
+                wordls.append(conv.do(i))
+            res = searchWord(wordls,ls)
+            if len(res) >= 1:
+                resWord = ""
+                for i in res[:3]:
+                    resWord += " [{}({}), {}]".format(ls[i[1][0]][0][i[1][1]],"".join(ls[i[1][0]][0]),i[0])
+                print("類似ワード: " + resWord)
+            else:
+                print("類似ワードなし")
+            print("")
+            ka.setMode('H', 'H')
+            conv = ka.getConverter()
+            word = input("サーチワード: ")
+            if word == "return":
+                break
+            word = conv.do(word)
+        print("")
+        name = input("登録:")
+        return cmd(name,ls,ka)
     else:
         return mes
+
+def check(word,ls):
+    wordch = "".join(word.split("１"))
+    for i in ls:
+        if "".join(i[0]) == wordch:
+            return False
+    return True
+
+def evalScore(word1,word2):
+    score = 0
+    if len(word1) == len(word2):
+        score += 2
+    for i in range(len(word2)):
+        if word1[i] == word2[i]:
+            score += 10
+        elif word1[i][-1] == word2[i][-1]:
+            score += 3
+        elif word1[i][:-1] == word2[i][:-1]:
+            score += 2
+        else:
+            score += -7
+    return score
+
+def maxScore(ls):
+    maxS = 1
+    result = []
+    for i in range(len(ls)):
+        for j in range(len(ls[i][0])):
+            if maxS <= ls[i][1][j]:
+                maxS = ls[i][1][j]
+                result.append([maxS,[i,j]])
+    result.sort(key=lambda x :x[0],reverse=True)
+    #print(result)
+    return result
+
+def searchWord(mes,ls):
+    scoreLs = []
+    for i in range(len(ls)):
+        scoreLs.append([ls[i][0],[]])
+        for j in range(len(ls[i][0])):
+            score = 0
+            if len(ls[i][1][j]) >= len(mes):
+                score += evalScore(ls[i][1][j],mes)
+                scoreLs[i][1].append(score)
+            else:
+                scoreLs[i][1].append(0)
+    result = maxScore(scoreLs)
+    return result
 
 def main():
     ls = []
@@ -41,32 +127,39 @@ def main():
     try:
         file = open("data.pickle",'wb')
         ka = kakasi()
-        name = cmd(input("名前:"),ls)
         ka.setMode('J', 'H')
         ka.setMode('K', 'H')
         ka.setMode('H', 'H')
         conv = ka.getConverter()
+        name = cmd(input("登録:"),ls,ka)
         while(name != "end"):
-            ka.setMode('H', 'H')
-            conv = ka.getConverter()
-            jName = (conv.do(name)).split("１")
-            ans = input("読み：{} ? input:".format(jName))
-            if False == (ans == 'y' or ans == 'ｙ' or ans == ""):
-                jName = ans.split("１")
-                print(jName)
-            ka.setMode('H', 'a')
-            conv = ka.getConverter()
-            tmpls = []
-            for i in jName:
-                tmp = []
-                for j in i:
-                    tmp.append(conv.do(j))
-                tmpls.append(tmp)
-            name = name.split("１")
-            ls.append([name,tmpls])
-            print("appended:",name,tmpls)
+            if check(name,ls) == True:
+                ka.setMode('H', 'H')
+                conv = ka.getConverter()
+                jName = (conv.do(name)).split("１")
+                ans = input("読み：{} ? input:".format(jName))
+                if False == (ans == 'y' or ans == 'ｙ' or ans == ""):
+                    jName = ans.split("１")
+                    if (ans != "cancel"):
+                        print(jName)
+                ka.setMode('H', 'a')
+                conv = ka.getConverter()
+                tmpls = []
+                for i in jName:
+                    tmp = []
+                    for j in i:
+                        tmp.append(conv.do(j))
+                    tmpls.append(tmp)
+                name = name.split("１")
+                if (ans != "cancel"):
+                    ls.append([name,tmpls])
+                    print("appended:",name,tmpls)
+                else:
+                    print("canceled")
+            else:
+                print("data exist.")
             print("")
-            name = cmd(input("名前:"),ls)
+            name = cmd(input("登録:"),ls,ka)
         pickle.dump(ls,file)
         f.close()
         print("")
@@ -77,8 +170,8 @@ def main():
         pickle.dump(ls,file)
         f.close()
         print("Error occured.")
-        for i in ls:
-           print(i)
+        #for i in ls:
+        #   print(i)
     
 
 if __name__ == '__main__':
